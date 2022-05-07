0d00eeb94d44ad607b0e39a033b2001fb31a44c9
Florian Labarre <florian.labarre@epitech.eu>
2022-05-07 18:51:17 +0200

    Fixed a bug with the search bar

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -6,6 +6,7 @@ import { Autocomplete, TextField } from '@mui/material';
 
 const Map = (): JSX.Element => {
     const [data, setData] = useState(schoolData);
+    const [searchValue, setSearchValue] = useState("");
 
     const updateData = (value: string | null) => {
         if (value == null)
@@ -17,21 +18,33 @@ const Map = (): JSX.Element => {
         }
     }
 
+    useEffect(() => {
+        updateData(searchValue);
+    }, [searchValue])
+
     return (
         <div>
             <Autocomplete
                 id="searchBar"
                 options={schoolData.map((option) => option.name)}
-                renderInput={(params) => <TextField {...params} label="Search" onChange={(e) => updateData(e.target.value)} />}
+                renderInput={(params) =>
+                    <TextField
+                        {...params}
+                        label="Search"
+                        onChange={(e) => setSearchValue(e.target.value)}
+                        value={searchValue}
+                    />
+                }
                 onChange={(event: any, newValue: string | null) => {
                     updateData(newValue);
                 }}
+                value={searchValue}
             />
             <MapContainer
                 center={[schoolData[0].latitude, schoolData[0].longitude]}
                 zoom={13}
                 scrollWheelZoom={true}
-                style={{ height: '94vh', width: '100wh' }}>
+                style={{ height: '94vh', width: '100vw' }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
